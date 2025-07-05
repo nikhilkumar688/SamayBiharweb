62d52b40780c2c2e632c72626b21f19878495c5b
Nikhil Kumar <your_email@example.com>
2025-07-05 23:43:50 +0530

    add a middleware to handle possible error

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,12 @@ app.listen(5000, () => {
   console.log("server is running on port 5000!");
 });
 app.use("/api/auth", authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
