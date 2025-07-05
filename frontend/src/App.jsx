fdc7b124e715db1c57a9a9d724a2af47d48e1a27
Nikhil Kumar <your_email@example.com>
2025-07-05 15:05:14 +0530

    installing shadcn ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-500">समय Bihar !</h1>
+      <Button className="bg-red-800" variant="ghost">
+        Click me
+      </Button>
     </div>
   );
 };
