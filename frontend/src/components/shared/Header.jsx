9759f0baddbc194f6733e4f66c19f3e4220b0879
Nikhil Kumar <your_email@example.com>
2025-07-05 20:37:41 +0530

    Add navbar component

---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <header className="shadow-lg sticky">
+      <div className="flex justify-between items-center max-w-6xl lg:max-w-7xl mx-auto p-4">
+        <Link to={"/"}>
+          <img
+            src="/logo.svg"
+            alt="समय Bihar Logo"
+            className="h-10 sm:h-14 w-auto object-contain "
+          />
+        </Link>
+        <form className="p-3 bg-slate-100 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="focus:outline-none bg-transparent w-24 sm:w-64"
+          />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to={"/"}>
+            <li className="hidden lg:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+
+          <Link to={"/about"}>
+            <li className="hidden lg:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+
+          <Link to={"/news"}>
+            <li className="hidden lg:inline text-slate-700 hover:underline">
+              News Articles
+            </li>
+          </Link>
+        </ul>
+        <Link to={"/sign-in"}>
+          <Button>Sign In</Button>
+        </Link>
+      </div>
+    </header>
+  );
+};
+export default Header;
