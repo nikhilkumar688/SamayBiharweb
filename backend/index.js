141585cca991000067467011324283ecef7d0e94
Nikhil Kumar <your_email@example.com>
2025-07-05 22:19:32 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
 app.listen(5000, () => {
   console.log("server is running on port 5000!");
 });
-console.log("Express app initialized.");
